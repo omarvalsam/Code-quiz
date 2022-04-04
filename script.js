73caa2b67e8f0df82cdb2dbb14f6b4aad903747d
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-04-03 22:04:14 -0600

    added more javascript for questions

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 const startButton = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById("question-container");
 
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
 startButton.addEventListener("click", startgame);
 // start the game
 function startgame() {
   startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 }
 
 // show next question
-function setNextQuestion() {}
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
 
 //choose a new question
 function selectAnswer() {}
+
+const questions = [
+  {
+    question: "Who is the best artist?",
+    answers: [
+      { text: "Rihanna", correct: true },
+      { text: "Arianna", correct: false },
+    ],
+  },
+];
