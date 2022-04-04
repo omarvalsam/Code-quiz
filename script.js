c68c692fb039cf24eec9873ad6e5d786a76c44be
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-04-03 22:40:45 -0600

    added functionality for questions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 
 const questionElement = document.getElementById("question");
@@ -7,6 +8,11 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startgame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
 // start the game
 function startgame() {
   startButton.classList.add("hide");
@@ -18,6 +24,7 @@ function startgame() {
 
 // show next question
 function setNextQuestion() {
+  resetState();
   showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
@@ -35,8 +42,43 @@ function showQuestion(question) {
   });
 }
 
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
 //choose a new question
-function selectAnswer() {}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 const questions = [
   {
@@ -44,6 +86,46 @@ const questions = [
     answers: [
       { text: "Rihanna", correct: true },
       { text: "Arianna", correct: false },
+    ],
+  },
+  {
+    question: "Who is the best 2nd artist?",
+    answers: [
+      { text: "Rihanna", correct: false },
+      { text: "Arianna", correct: true },
+    ],
+  },
+  {
+    question: "if a woodchuck could chuck wood how much wood would he chuck?",
+    answers: [
+      { text: "WHO CARES!", correct: false },
+      { text: "I DUNNO?!", correct: false },
+      { text: "the limit does not exist", correct: true },
+      { text: "2", correct: false },
+    ],
+  },
+  {
+    question: "Is coding fun?",
+    answers: [
+      { text: "I DUNNO?!", correct: false },
+      { text: "I GUESSSSS", correct: false },
+      { text: "yes", correct: true },
+    ],
+  },
+  {
+    question: "is the timer on this game making you nervous?",
+    answers: [
+      { text: "yes", correct: true },
+      { text: "no", correct: true },
+    ],
+  },
+  {
+    question: "whats the best type of video game genre?",
+    answers: [
+      { text: "ALL OF THE ABOVE !!!", correct: true },
+      { text: "FIGHTING", correct: false },
+      { text: "RPG", correct: false },
+      { text: "FPS", correct: false },
     ],
   },
 ];
